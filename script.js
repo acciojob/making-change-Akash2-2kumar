87ec8d04c15337a987de460ccaff46180ec787e7
Akash2-2kumar <106278593+Akash2-2kumar@users.noreply.github.com>
2026-08-20 13:33:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
+class Change{
+    constructor(q,d,n,p){
+        this.q=q;
+        this.d=d;
+        this.n=n;
+        this.p=p;
+    }
+}
+
 const makeChange = (c) => {
-  // your name here
+  // your name here Penny: 0.01
+  // Nickel: 0.05
+  // Dime: 0.10
+  // Quarter: 0.25
+  let p=1;
+  let n=5;
+  let d=10;
+  let q=25;
+  
+  let cp=0;
+  let cn=0;
+  let cd=0;
+  let cq=0;
+  cq=Math.floor(c/q);
+  c=c%q;
+  cd=Math.floor(c/d);
+  c=c%d;
+  cn=Math.floor(c/n);
+  c=c%n;
+  cq=Math.floor(c/q);
+  return new Change(cq,cd,cn,cp);
+  
 };
 
 // Do not the change the code below
